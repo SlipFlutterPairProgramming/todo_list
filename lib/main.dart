675a947859875eae6401a5fc2e1ac09fdd13460e
bernice75 <good12388@daum.net>
2023-11-09 20:36:46 +0900

    ml-1

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -25,8 +26,65 @@ class MyApp extends StatelessWidget {
 class TodoAppWidget extends StatelessWidget {
   const TodoAppWidget({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+}
+
+class TodoController extends GetxController {
+  final list = [].obs;
+}
+
+enum Group {toDo, toSchedule, toDelegate, toDelete}
+(Color, Color) getGroupColors(Group group) {
+  switch(group) {
+    case Group.toDo:
+      return (
+        Color.fromRGBO(255, 129, 129, 1),
+        Color.fromRGBO(208, 244, 164, 1)
+      );
+    case Group.toSchedule:
+      return (
+        Color.fromRGBO(208, 244, 164, 1),
+        Color.fromRGBO(102, 119, 187, 1)
+      );
+    case Group.toDelegate:
+      return (
+        Color.fromRGBO(234, 255, 208, 1),
+        Color.fromRGBO(210, 151, 243, 1)
+      );
+    case Group.toDelete:
+      return (
+        Color.fromRGBO(149, 225, 211, 1),
+        Color.fromRGBO(226, 124, 127, 1)
+      );
+  }
+}
+
+class TodoCard extends StatefulWidget {
+  const TodoCard({super.key, required this.group, required this.content,});
+  final Group group;
+  final String content;
+
+  @override
+  State<TodoCard> createState() => _TodoCardState();
+}
+
+class _TodoCardState extends State<TodoCard> {
+
+  @override
+  Widget build(BuildContext context) {
+    final (bg, fg) = getGroupColors(widget.group);
+    return Row(
+      children: [
+        Expanded(child: Row(
+          children: [
+            
+          ],
+        ),),
+      ],
+    );
   }
 }
