6d996d7a057df5a0145b3c820c744de7eb2f5791
makerkingcat <makerkingcat@gmail.com>
2023-11-09 21:28:06 +0900

    Add favorite

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:uuid/uuid.dart';
 1. 
 2. getxcontoller debugging, tile 보이기
 3. 
-4. add tile 구현 및 라우팅
+4. 
 5. delete, done, favorite  fun 구현
 6. delete, done, favorite gestureDetector 구현
 7. favorite 을 정렬로 재구현
@@ -63,7 +63,7 @@ class TodobGroup extends GetView<TodobContorller> {
     return Expanded(
       flex: 100,
       child: GestureDetector(
-          onTap: () => Get.bottomSheet(const AddPage()),
+          onLongPress: () => Get.bottomSheet(const AddPage()),
           //     SingleChildScrollView(
           //   child: Column(
           //     children: [
@@ -100,6 +100,7 @@ class TodobTile extends StatefulWidget {
 
   final content;
   final group;
+  bool favorite = false;
   final uuid = uuidGen.v4();
   static const uuidGen = Uuid();
 
@@ -110,11 +111,17 @@ class TodobTile extends StatefulWidget {
 class _TodobTileState extends State<TodobTile> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const Icon(Icons.star),
-        Text(widget.content),
-      ],
+    return GestureDetector(
+      onDoubleTap: () => setState(() {
+        print('double tap');
+        widget.favorite = !widget.favorite;
+      }),
+      child: Row(
+        children: [
+          widget.favorite ? const Icon(Icons.star) : const Text(''),
+          Text(widget.content),
+        ],
+      ),
     );
   }
 }
@@ -130,7 +137,10 @@ class AddPage extends GetView<TodobContorller> {
         child: Column(
           children: [
             TextField(
-              onSubmitted: (text) => controller.addTile(text, Group.Todo),
+              onSubmitted: (text) {
+                controller.addTile(text, controller.addSelected.value);
+                Get.back();
+              },
             ),
             DropdownMenu<Group>(
               initialSelection: Group.Todo,
